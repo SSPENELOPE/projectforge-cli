298c69c2a3843bf72f3850b7f5ba6acd11e3e5fd
SSPENELOPE <typoepping@gmail.com>
2024-06-03 12:50:15 -0400

    updated react_tsx library to handle form validation properly

--- createDirectoryContent.js
+++ createDirectoryContent.js
@@ -10,13 +10,12 @@ const createDirectoryContents = (templatePath, newProjectPath) => {
   // Create a new progress bar instance with custom options
   const progressBar = new cliProgress.SingleBar(
     {
-      format:
-        "Progress |" +
+      format: "Progress |" +
         colors.cyan("{bar}") +
         "| {percentage}% | {value}/{total} Files | Current File: " +
         colors.yellow("{filename}"),
-      barCompleteChar: colors.green,
-      barIncompleteChar: colors.red,
+      barCompleteChar: colors.green('\u2588'),  // solid block character
+      barIncompleteChar: colors.red('\u2591'),  // light shade character
       barsize: 20, // Adjust barsize to make it smaller
       hideCursor: true,
     },
@@ -29,12 +28,12 @@ const createDirectoryContents = (templatePath, newProjectPath) => {
   });
 
   filesToCreate.forEach((file) => {
-    // Ignore node_modules directory, this allows us to get buy without installing removing and installing node_modules
+    // Ignore node_modules directory, this allows us to get by without installing removing and installing node_modules
     if (file === "node_modules" || file === ".npmignore") return;
 
     const origFilePath = `${templatePath}/${file}`;
 
-    // get stats about the current file
+    // Get stats about the current file
     const stats = fs.statSync(origFilePath);
 
     if (stats.isFile()) {
@@ -49,10 +48,7 @@ const createDirectoryContents = (templatePath, newProjectPath) => {
       fs.mkdirSync(`${CURR_DIR}/${newProjectPath}/${file}`);
 
       // Recursive call
-      createDirectoryContents(
-        `${templatePath}/${file}`,
-        `${newProjectPath}/${file}`
-      );
+      createDirectoryContents(`${templatePath}/${file}`, `${newProjectPath}/${file}`);
     }
 
     // Update the progress bar with the current file name
